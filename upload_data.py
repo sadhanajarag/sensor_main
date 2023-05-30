55013c1f75576505d262ca2900b22e39ca71a802
sadhanajarag <sadhana20.jaarg@gmail.com>
2023-05-29 19:09:32 -0500

    this is my first commit

---
+++ upload_data.py
@@ -0,0 +1,24 @@
+import pymongo
+from pymongo.mongo_client import MongoClient
+import pandas as pd
+import json
+
+# uniform resource indentifier
+uri = "mongodb+srv://sadhana20jarag:1234@cluster0.udydp5r.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# create database name and collection name
+DATABASE_NAME="pwskills"
+COLLECTION_NAME="waferfault"
+
+# read the data as a dataframe
+df=pd.read_csv(r"C:\DS_Material\Krish Naik_PW skills\ML PROJECT\sensor-main\notebooks\wafer_23012020_041211.csv")
+df=df.drop("Unnamed: 0",axis=1)
+
+# Convert the data into json
+json_record=list(json.loads(df.T.to_json()).values())
+
+#now dump the data into the database
+client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
